b07f65928f80ed01f698cf982444850d03fd58ba
daniel235 <daduncan298@gmail.com>
2018-12-01 17:47:16 -0600

    got phos data sorted

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 import tensorflow as tf
+import numpy as np
 import pandas as pd
 
 
 kinaseData = pd.read_csv('./data/Kinase_Substrates.txt', delimiter="\t")
 
 phosphorylation = pd.read_csv('./data/phosphorylation_data.txt', delimiter="\t")
-print(phosphorylation)
+
+#Phosphorylation name
+phosphoType = phosphorylation["Phosphosite"]
+
+phosphorylation = np.array(phosphorylation)
+
+phosDataX = []
+phosDataY = []
+
+
+#phosphorylation data separated with xname and x data
+for i in range(len(phosphorylation)):
+    for j in range(1, 19):
+        phosDataX.append([phosphorylation[i][0], phosphorylation[i][j]])
+
+
+#need to link y data
+
+
+
+
+
+
+
+
