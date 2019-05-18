dd5c75a687807647b021e2e9be4800a6053da80b
daniel235 <daduncan298@gmail.com>
2019-05-17 22:13:48 -0500

    adding node objects

--- graph.py
+++ graph.py
@@ -1,6 +1,3 @@
-
-        
-
 import pandas as pd
 
 class Graph:
@@ -8,34 +5,53 @@ class Graph:
         self.graph = None
         self.kinaseNodes = []
         self.kinaseData = pd.read_csv("./data/Kinase_Substrates.txt", delimiter="\t")
+        self.edges = []
 
-    def createNode(self):
-        pass
+
+    def createNode(self, name):
+        node = Node(name)
+        self.kinaseNodes.append(node)
+
 
     def add(self):
         pass
 
+
     def addEdge(self, node, substrate):
         #add substrate as edge 
-        pass
-        
+        #check if edge is in use
+        if substrate in self.edges:
+            index = self.edges.index(substrate)
+            #grab neighbor node 
+            myNode = self.kinaseNodes[index]
+
+        #no neighbor for node
+        else:
+            self.edges.append(substrate)
+            node.edges.append(substrate)
 
 
     def createGraph(self):
         for i in range(len(self.kinaseData)):
             if self.kinaseData["Kinase"][i] in self.kinaseNodes:
-                self.addEdge(self.kinaseNodes[self.kinaseData["Kinase"][i]], self.kinaseData["Substrate"][i])
+                node = self.kinaseNodes.index(self.kinaseData["Kinase"][i])
+                self.addEdge(node, self.kinaseData["Substrate"][i])
 
             else:
-                self.createNode()
+                self.createNode(self.kinaseData["Kinase"][i])
+
 
 class Node:
-    def __init__(self):
-        self.name = ""
+    def __init__(self, name):
+        self.name = name
         self.edges = []
+        self.neighbors = []
 
     def add(self, substrate):
         self.edges.append(substrate)
+
+    def addNeighbor(self, node):
+        pass
             
         
 
