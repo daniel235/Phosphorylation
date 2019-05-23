51bc445ebc3a5417b06d9c566bb30823d5d877fa
daniel235 <daduncan298@gmail.com>
2019-05-23 00:54:42 -0500

    update

--- graph.py
+++ graph.py
@@ -1,57 +1,106 @@
 import pandas as pd
 
+
 class Graph:
+    '''
+        This class creates the graph using the weight of edges
+        as # of substrates kinases share.
+
+        graph class includes nodes edges
+
+        node class uses name it's edges and neighbors
+
+        edge class uses substrates names and number of edges.
+    '''
     def __init__(self):
-        self.graph = None
+        self.graph = []
         self.kinaseNodes = []
+        self.kinaseNodesNames = []
         self.kinaseData = pd.read_csv("./data/Kinase_Substrates.txt", delimiter="\t")
         self.edges = []
+        self.edgeNames = []
 
 
     def createNode(self, name):
         node = Node(name)
         self.kinaseNodes.append(node)
+        self.kinaseNodesNames.append(node.name)
+        return node
 
 
-    def add(self):
-        pass
+    def add(self, node):
+        self.graph.append(node)
 
 
-    def addEdge(self, node, substrate):
-        #add substrate as edge 
-        #check if edge is in use
-        if substrate in self.edges:
-            index = self.edges.index(substrate)
-            #grab neighbor node 
-            myNode = self.kinaseNodes[index]
+    def addEdge(self, substrate):
+        self.edgeNames.append(substrate)
 
-        #no neighbor for node
+        #create edge
+        e = Edge(substrate)
+        self.edges.append(e)
+
+        return e 
+
+    
+    def grabEdge(self, substrate):
+        if substrate in self.edgeNames:
+            index = self.edgeNames.index(substrate)
+            edge = self.edges[index]
+
         else:
-            self.edges.append(substrate)
-            node.edges.append(substrate)
+            edge = self.addEdge(substrate)
+
+        return edge
+
 
 
     def createGraph(self):
         for i in range(len(self.kinaseData)):
-            if self.kinaseData["Kinase"][i] in self.kinaseNodes:
-                node = self.kinaseNodes.index(self.kinaseData["Kinase"][i])
-                self.addEdge(node, self.kinaseData["Substrate"][i])
+            #grab substrate object
+            s = self.kinaseData[i][1]
+            s = self.grabEdge(s)
+
+            #check if kinase in nodes
+            if self.kinaseData["Kinase"][i] in self.kinaseNodesNames:
+                nodeIndex = self.kinaseNodesNames.index(self.kinaseData["Kinase"][i])
+                node = self.kinaseNodes[nodeIndex]
 
             else:
-                self.createNode(self.kinaseData["Kinase"][i])
+                node = self.createNode(self.kinaseData["Kinase"][i])
+
+            #grab nodes associated with this substrate
+            subNodes = s.nodes
+
+            #create neighbors or add neighbors to this node
+            for i in range(len(subNodes)):
+                if subNodes[i] in node.neighbors:
+                    node.neighbors[subNodes[i]] += 1
+                else:
+                    node.neighbors[subNodes[i]] = 1
+
+                
+        
+
 
 
 class Node:
     def __init__(self, name):
         self.name = name
         self.edges = []
-        self.neighbors = []
+        self.neighbors = {}
 
     def add(self, substrate):
-        self.edges.append(substrate)
+        edge = Edge(substrate)
+        self.edges.append(edge)
 
-    def addNeighbor(self, node):
-        pass
             
         
 
+class Edge:
+    def __init__(self, name):
+        self.name = name
+        self.nodes = []
+
+    #add node object 
+    def addNode(self, node):
+        self.nodes.append(node)
