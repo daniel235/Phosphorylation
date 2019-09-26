7e98948162983aba7535849cc266c3f4cbbbe551
daniel acevedo <daduncan298@gmail.com>
2019-09-26 11:20:51 -0500

    update

--- alias.py
+++ alias.py
@@ -4,8 +4,10 @@ import pandas as pd
 class Alias:
     def __init__(self, filename):
         self.data = None
+        self.alias_dict = {}
+        self.alias_list = []
         self.get_alias_file(filename)
-
+        
 
     def get_alias_file(self, filename):
         filename = "./data/info_table.csv"
@@ -26,7 +28,9 @@ class Alias:
                     kinase = ""
             #turn string into a list of kinases
             self.data['Alias'][i] = kinases
+            kinases = []
 
+        self.set_alias_dictionary()
 
     #filter through all kinases in file return main kinase
     def get_main_kinase(self, kinase):
@@ -36,10 +40,36 @@ class Alias:
                 return kinase
 
         #if not found go through every row of kinases
-        for i in range(len(self.data)):
-            for j in range(len(self.data['alias'][i])):
-                if kinase.upper() == self.data['alias'][i][j].upper():
-                    return self.data['Gene'][i]
+        #binary search sorted array
+        m = len(self.alias_list) / 2
+        current = None
+        low = 0
+        high = len(self.alias_list)
+        while(m != current):
+            current = self.alias_list[m]
+            #check if kinase is current kinase pointed to
+            if kinase == current:
+                break
+            #check if kinase name is in lower half
+            if kinase < current:
+                m = m / 2
+            #check if kinase name is in upper half
+            else:
+                m = m + (m / 2)
+
+
+
 
         return kinase
+
+    
+    def set_alias_dictionary(self):
+        for i in range(len(self.data)):
+            for alias in self.data['Alias'][i]:
+                #print(alias, "len ", len(self.data['Alias'][i]))
+                self.alias_dict[alias] = self.data['Gene'][i]
             
+        #sort dictionary
+        self.alias_list = sorted(self.alias_dict.keys())
+        print(self.alias_dict)
+        
